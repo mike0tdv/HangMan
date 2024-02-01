c1ff18b7a9a3a343f062d993e0d6d63ce331fa9e
mike0tdv <mihailtodev3@gmail.com>
2024-02-02 00:37:30 +0200

    Testing

--- Main.py
+++ Main.py
@@ -11,16 +11,29 @@ diffEasy = False
 diffMid = False
 diffHard = False
 diffFalse = True
+guessed = False
+countRight = 0
+countFalse = 0
+countRight1 = 0
 letters = []
+word = []
 def wordGen(x):
     rnd = random.randint(0, 4)
     letters = list(words[x][rnd])
     print("This is your word: ")
     for i in range(len(letters)):
-        print("_", end="")
+        word.append("_")
+        print(word[i], end="")
 
-
-
+def letterCheck(y, x, z, x1):
+    for i in range(len(letters)):
+        if letters[i] == y:
+            word[i] = y
+            x1 = x + 1
+    if x1 == x:
+        z += 1
+    else:
+        x == x1
 
 playAgain = True
 while playAgain:
@@ -40,13 +53,39 @@ while playAgain:
         elif diff == "hard":
             diffHard = True
             break
-        else: print("There is no such difficulty!")
+        else:
+            print("There is no such difficulty!")
 
-    while diffEasy == True:
+    while diffEasy:
         playAgain = False
-        diffEasy = False
-        print("You have thr right for 10 mistakes. After this the game ends!")
+        print("You have the right for 10 mistakes. After this the game ends!")
         print("The game will now choose a word...")
         pyautogui.countdown(3)
         print()
         wordGen(0)
+        print()
+        diffEasy = False
+
+        while not guessed:
+            diffEasy = False
+            guess = input("Give your guess: ")
+            letterCheck(guess, countRight, countRight1, countFalse)
+            for i in range(len(letters)):
+                word.append("_")
+                print(word[i], end="")
+
+            if countRight == len(letters) + 1:
+                guessed = True
+                print("You Win!")
+                pa = (input("Do you want to play again(True/False): ")).lower()
+                if pa == "true":
+                    playAgain = True
+                break
+            elif countFalse == 10:
+                guessed = True
+                print("You lose!")
+                pa = (input("Do you want to play again(True/False): ")).lower()
+                if pa == "true":
+                    playAgain = True
+                break
+
