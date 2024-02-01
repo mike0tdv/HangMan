0a098266b4730cbd9bc8b6f32c1b8e111f584881
mike0tdv <mihailtodev3@gmail.com>
2024-02-02 00:07:42 +0200

    Half of easy is ready

--- Main.py
+++ Main.py
@@ -4,21 +4,49 @@ import pyautogui
 fraudWords = ["log", "word", "phone", "board", "mouse"]
 midWords = ["block", "poster", "lumber", "marker", "blanket"]
 HimWords = ["keyboard", "parliament", "ministry", "infrastructure", "internal"]
-nums = [0, 1, 2, 3, 4]
+
+words = [fraudWords, midWords, HimWords]
+
+diffEasy = False
+diffMid = False
+diffHard = False
+diffFalse = True
+letters = []
+def wordGen(x):
+    rnd = random.randint(0, 4)
+    letters = list(words[x][rnd])
+    print("This is your word: ")
+    for i in range(len(letters)):
+        print("_", end="")
+
+
+
 
 playAgain = True
 while playAgain:
+
     print("This is a Hang-Man game!")
     print("Choose a difficulty between the following: Easy, Medium, Hard")
-    diff = (input("Choose your difficulty: ")).lower()
-    randm = random.choice(nums)
 
-    while diff == "easy":
+    while diffFalse:
+        diff = (input("Choose your difficulty: ")).lower()
+
+        if diff == "easy":
+            diffEasy = True
+            break
+        elif diff == "medium":
+            diffMid = True
+            break
+        elif diff == "hard":
+            diffHard = True
+            break
+        else: print("There is no such difficulty!")
+
+    while diffEasy == True:
+        playAgain = False
+        diffEasy = False
         print("You have thr right for 10 mistakes. After this the game ends!")
         print("The game will now choose a word...")
-
-        letters = list(fraudWords[randm])
         pyautogui.countdown(3)
-
-        for i in range(len(letters)):
-            print("_", end="")
+        print()
+        wordGen(0)
