8f6ba01f6002151954677e218859a104627e39b6
mike0tdv <mihailtodev3@gmail.com>
2024-02-13 01:37:42 +0200

    Testing

--- Test_Main.py
+++ Test_Main.py
@@ -13,6 +13,7 @@ mid = 1
 hard = 2
 
 count = 0
+count1 = 0
 mistakes = 0
 mistakes1 = 0
 
@@ -21,30 +22,58 @@ letters = []
 rnd = 0
 i = ""
 j = 0
+k = 0
+
 # a function to generate a random word no matter the difficulty
 def wordGen(y):
     global word
     global letters
     global rnd
+
     rnd = random.randint(0, 4)
-    word = words[y][rnd]
+    word = words[y][0]
     letters = list(word)
 
 # function that prints the word using only a "-" symbol
 def printWord():
     global i
     global word
+
     for i in word:
         print("-", end="")
     print()
 
+# function that fils up a list with "-" so they can be changed into the chosen letters
 def guessFill():
     global guesses
     global word
     global j
+
     guesses = list(range(len(word)))
     for j in range(len(word)):
         guesses[j] = "-"
+
+# function for checking if the letter the user typed is part of the word
+def guessLoop(guess, count1):
+    global word
+    global i
+    global j
+    global k
+    global count
+
+    for j in range(len(word)):
+        if guess == word[j]:
+            guesses[j] = guess
+            for k in guesses:
+                if k == "-":
+                    count += 1
+            print("You guessed!")
+            for i in guesses:
+                print(i, end="")
+            print()
+    print(f"Letters left to guess: {count1 - count}")
+    print()
+
 
 # choosing a difficulty
 diff = input("Enter a difficulty(easy, normal, hard): ").lower()
@@ -55,57 +84,78 @@ if diff == "easy":
     print("You have chosen the easy difficulty!")
     print("*********************************")
     print("You have the right of 10 mistakes! After that the game ends!")
-    pyautogui.countdown(3)
+    pyautogui.countdown(0)
     print()
 
     wordGen(easy)
 
-    # making a counter for the guessed letters
-    count = len(letters)
-    # rnd = random.randint(0, 4)
-    # word = words[y][rnd]
-    # letters = list(word)
+    # a counter for the guessed letters
+    count1 = len(letters)
 
     # printing the word using only a "-" symbol
     printWord()
-    # for i in word:
-    #     print("-", end="")
-    # print()
 
     # filling up a list with "-" so they can be changed into the chosen letters
-    guesses = list(range(len(word)))
-    for i in range(len(word)):
-        guesses[i] = "-"
+    guessFill()
 
     # a while loop for the user to guess the word
-    while count != 0:
+    while count != count1:
         guess = input("Enter a guess: ")
+        guessLoop(guess, count1)
 
-        for i in range(len(word)):
-            if guess == word[i]:
-                count -= 1
-                guesses[i] = guess
-                print("You guessed!")
-                for j in guesses:
-                    print(j, end="")
-            else:
-                mistakes1 += 1
-        print()
-        print(f"Letters to be guessed left: {count} ")
-        # print(f"Mistakes left: {mistakes1}")
-        print()
-        # if mistakes1 > mistakes:
-        #     mistakes = mistakes1
-        #     for j in guesses:
-        #         print(j, end="")
-        # print()
     print(f"Congratulations! You guessed the word '{word}'")
 
 # normal difficulty
 elif diff == "normal":
     print("You have chosen the normal difficulty!")
+    print("*********************************")
+    print("You have the right of 7 mistakes! After that the game ends!")
+    pyautogui.countdown(3)
+    print()
+
+    wordGen(mid)
+
+    # a counter for the guessed letters
+    count = len(letters)
+
+    # printing the word using only a "-" symbol
+    printWord()
+
+    # filling up a list with "-" so they can be changed into the chosen letters
+    guessFill()
+
+    # a while loop for the user to guess the word
+    while count != 0:
+        guess = input("Enter a guess: ")
+        guessLoop(guess, count)
+
+    print(f"Congratulations! You guessed the word '{word}'")
+
 
 # hard difficulty
 elif diff == "hard":
     print("You have chosen the hard difficulty!")
+    print("*********************************")
+    print("You have the right of 4 mistakes! After that the game ends!")
+    pyautogui.countdown(3)
+    print()
+
+    wordGen(hard)
+
+    # a counter for the guessed letters
+    count = len(letters)
+
+    # printing the word using only a "-" symbol
+    printWord()
+
+    # filling up a list with "-" so they can be changed into the chosen letters
+    guessFill()
+
+    # a while loop for the user to guess the word
+    while count != 0:
+        guess = input("Enter a guess: ")
+        guessLoop(guess, count)
+
+    print(f"Congratulations! You guessed the word '{word}'")
+
 
