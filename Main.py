d8c3c7ad05a93d3915b66b57551edd722e7fa8d7
mike0tdv <mihailtodev3@gmail.com>
2024-02-13 23:30:05 +0200

    Finished game

--- Main.py
+++ Main.py
@@ -1,99 +1,196 @@
 import random
 import pyautogui
 
-fraudWords = ["log", "word", "phone", "board", "mouse"]
+# making a list of all possible words with different difficulties
+easyWords = ["log", "word", "phone", "board", "mouse"]
 midWords = ["block", "poster", "lumber", "marker", "blanket"]
-HimWords = ["keyboard", "parliament", "ministry", "infrastructure", "internal"]
+hardWords = ["keyboard", "parliament", "ministry", "infrastructure", "internal"]
+words = [easyWords, midWords, hardWords]
 
-words = [fraudWords, midWords, HimWords]
+# declaring variables to use later
+easy = 0
+mid = 1
+hard = 2
 
-diffEasy = False
-diffMid = False
-diffHard = False
-diffFalse = True
-guessed = False
-countRight = 0
-countFalse = 0
-countRight1 = 0
+count = 0
+count1 = 0
+mistakes = 0
+mistakes1 = 0
+temp = 0
+
+word = ""
 letters = []
-word = []
-def wordGen(x):
-    rnd = random.randint(0, 4)
-    letters = list(words[x][rnd])
-    print("This is your word: ")
-    for i in range(len(letters)):
-        word.append("_")
-        print(word[i], end="")
-
-def letterCheck(y, x, z, x1):
-    for i in range(len(letters)):
-        if letters[i] == y:
-            word[i] = y
-            x1 = x + 1
-    if x1 == x:
-        z += 1
-    else:
-        x = x1
-
+rnd = 0
+i = ""
+j = 0
+k = 0
 playAgain = True
+pa = ""
+
+# a function to generate a random word no matter the difficulty
+def wordGen(y):
+    global word, letters, rnd
+
+    rnd = random.randint(0, 4)
+    word = words[y][rnd]
+    letters = list(word)
+
+# function that prints the word using only a "-" symbol
+def printWord():
+    global i, word
+
+    for i in word:
+        print("-", end="")
+    print()
+
+# function that fills up a list with "-" so they can be changed into the chosen letters
+def guessFill():
+    global guesses, word, j
+
+    guesses = list(range(len(word)))
+    for j in range(len(word)):
+        guesses[j] = "-"
+
+# function for checking if the letter the user typed is part of the word
+def guessLoop(guess, mistakes1):
+    global word, i, j, k, count, letters, temp, mistakes
+
+    temp = count
+    for j in range(len(letters)):
+        if guess == letters[j]:
+            guesses[j] = guess
+            count -= 1
+            print("You guessed!")
+            for i in guesses:
+                print(i, end="")
+            print()
+    if count == temp:
+        mistakes += 1
+    print(f"Letters left to guess: {count}")
+    print(f"Mistakes: {mistakes}/{mistakes1}")
+    print()
+
 while playAgain:
+    # choosing a difficulty
+    diff = input("Enter a difficulty(easy, normal, hard): ").lower()
 
-    print("This is a Hang-Man game!")
-    print("Choose a difficulty between the following: Easy, Medium, Hard")
+    # easy difficulty
+    if diff == "easy":
+        print()
+        print("You have chosen the easy difficulty!")
+        print("*********************************")
+        print("You have the right of 10 mistakes! After that the game ends!")
+        pyautogui.countdown(3)
+        print()
 
-    while diffFalse:
-        diff = (input("Choose your difficulty: ")).lower()
+        mistakes1 = 10
+        wordGen(easy)
 
-        if diff == "easy":
-            diffEasy = True
-            break
-        elif diff == "medium":
-            diffMid = True
-            break
-        elif diff == "hard":
-            diffHard = True
-            break
+        # a counter for the guessed letters
+        count = len(word)
+
+        # printing the word using only a "-" symbol
+        printWord()
+
+        # filling up a list with "-" so they can be changed into the chosen letters
+        guessFill()
+
+        # a while loop for the user to guess the word
+        while count > 0:
+            guess = input("Enter a guess: ")
+            guessLoop(guess, mistakes1)
+            if mistakes == mistakes1:
+                print("You made too many mistakes!")
+                break
+
+        print(f"The word was '{word}'! You finished with {mistakes} mistakes")
+        print()
+        pa = input("Do you want to play again(Y/N): ").lower()
+        if pa == "n":
+            playAgain = False
+        elif pa == "y":
+            print()
         else:
-            print("There is no such difficulty!")
+            print("This is not an option!")
+            print("Goodbye")
+            playAgain = False
 
-    while diffEasy:
-        playAgain = False
-        print("You have the right for 10 mistakes. After this the game ends!")
-        print("The game will now choose a word...")
-        pyautogui.countdown(1)
+    # normal difficulty
+    elif diff == "normal":
+        print("You have chosen the normal difficulty!")
+        print("*********************************")
+        print("You have the right of 7 mistakes! After that the game ends!")
+        pyautogui.countdown(3)
         print()
-        wordGen(0)
-        print()
-        diffEasy = False
 
-        while not guessed:
-            diffEasy = False
-            guess = input("Give your guess: ")
-            # letterCheck(guess, countRight, countRight1, countFalse)
-            for i in range(len(letters)):
-                if letters[i] == guess:
-                    word[i] = guess
-                    countRight1 = countRight + 1
-            # if countRight1 == countRight:
-            #     countFalse += 1
-            else:
-                countRight = countRight1
+        mistakes1 = 7
+        wordGen(mid)
 
-            for i in range(len(letters)):
-                print(word[i], end="")
+        # a counter for the guessed letters
+        count = len(letters)
 
-            if countRight == len(letters) + 1:
-                guessed = True
-                print("You Win!")
-                pa = (input("Do you want to play again(True/False): ")).lower()
-                if pa == "true":
-                    playAgain = True
-                break
-            elif countFalse == 10:
-                guessed = True
-                print("You lose!")
-                pa = (input("Do you want to play again(True/False): ")).lower()
-                if pa == "true":
-                    playAgain = True
+        # printing the word using only a "-" symbol
+        printWord()
+
+        # filling up a list with "-" so they can be changed into the chosen letters
+        guessFill()
+
+        # a while loop for the user to guess the word
+        while count > 0:
+            guess = input("Enter a guess: ")
+            guessLoop(guess, mistakes1)
+            if mistakes == mistakes1:
+                print("You made too many mistakes!")
                 break
 
+        print(f"The word was '{word}'! You finished with {mistakes} mistakes")
+        print()
+        pa = input("Do you want to play again(Y/N): ").lower()
+        if pa == "n":
+            playAgain = False
+        elif pa == "y":
+            print()
+        else:
+            print("This is not an option!")
+            print("Goodbye")
+            playAgain = False
+
+    # hard difficulty
+    elif diff == "hard":
+        print("You have chosen the hard difficulty!")
+        print("*********************************")
+        print("You have the right of 4 mistakes! After that the game ends!")
+        pyautogui.countdown(3)
+        print()
+
+        mistakes1 = 4
+        wordGen(hard)
+
+        # a counter for the guessed letters
+        count = len(letters)
+
+        # printing the word using only a "-" symbol
+        printWord()
+
+        # filling up a list with "-" so they can be changed into the chosen letters
+        guessFill()
+
+        # a while loop for the user to guess the word
+        while count > 0:
+            guess = input("Enter a guess: ")
+            guessLoop(guess, mistakes1)
+            if mistakes == mistakes1:
+                print("You made too many mistakes!")
+                break
+
+        print(f"The word was '{word}'! You finished with {mistakes} mistakes")
+        print()
+        pa = input("Do you want to play again(Y/N): ").lower()
+        if pa == "n":
+            playAgain = False
+        elif pa == "y":
+            print()
+        else:
+            print("This is not an option!")
+            print("Goodbye")
+            playAgain = False
